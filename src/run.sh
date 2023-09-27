9a9616ee43e8cc15cf3042b32d6a479e73b8af93
yang02.gao <yang02.gao@horizon.ai>
2023-09-27 11:27:56 +0800

    feat(cmdb): [cr_id_skip] Change mongo url

--- src/run.sh
+++ src/run.sh
@@ -11,7 +11,7 @@ then
 fi
 
 cd /data/cmdb
-python init.py --discovery 127.0.0.1:2181 --database cmdb --redis_ip 127.0.0.1 --redis_port 6379 --redis_pass cc --mongo_ip 127.0.0.1 --mongo_port 27017 --mongo_user cc --mongo_pass cc --blueking_cmdb_url http://carizon.cmdb.aidi-dev.hobot.cc/ --blueking_paas_url http://carizon.cmdb.aidi-dev.hobot.cc/ --listen_port 8090 --user_info admin:admin --auth_enabled false --auth_login_version ldap
+python init.py --discovery 127.0.0.1:2181 --database cmdb --redis_ip 127.0.0.1 --redis_port 6379 --redis_pass cc --mongo_ip 10.34.64.5:27017,10.34.64.6:27017,10.34.64.7:27017 --mongo_port 27017 --mongo_user cc_wr --mongo_pass Golxr5Gf8dbSvqpnT --rs_name=rs01 --blueking_cmdb_url http://carizon.cmdb.aidi-dev.hobot.cc --blueking_paas_url http://carizon.cmdb.aidi-dev.hobot.cc --listen_port 8090 --user_info admin:admin --auth_enabled false --auth_login_version ldap
 
 # skip-login mode
 cd /data/cmdb/cmdb_adminserver/configures/
